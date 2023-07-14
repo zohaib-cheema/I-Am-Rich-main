d030b343dddf73c5b3f0fb399a27ea7ab5488b53
Zohaib Cheema <134620649+zohaib-cheema@users.noreply.github.com>
2023-07-14 15:53:38 -0400

    Add files via upload

---
+++ I-Am-Rich-main/README.md
@@ -0,0 +1,3 @@
+# I-Am-Rich
+
+Created the infamous I Am Rich IOS application as an introductory project! Application simply contains an imported picture and app icons of flexible dimensions, a label text, and button for reference all in the main storyboard!
---
+++ I-Am-Rich-main/Source-Code/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Moeez Khan on 6/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
